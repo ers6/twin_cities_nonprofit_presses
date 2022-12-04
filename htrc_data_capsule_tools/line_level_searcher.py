220cc512da39fface901c11a337a879f8470fed1
ers6 <98556588+ers6@users.noreply.github.com>
2022-12-04 15:13:27 -0600

    Update line_level_searcher.py

--- htrc_data_capsule_tools/line_level_searcher.py
+++ htrc_data_capsule_tools/line_level_searcher.py
@@ -14,13 +14,13 @@ def main():
             infile_name = str(f)+"/"+file_name +".txt"
 #             enter your search terms here -- returns lines with any of the terms provided in the list
             search_terms = ["nonprofit"]
-            these_results.append(line_level_concordance_search(infile_name, search_terms))
+            these_results.append(line_level_concordance_search(infile_name, search_terms, mode="all"))
     makes_results_csv(these_results, "results.csv")
 
 
 
 #this version allows for multiple search terms (copyright, donors, foundation, etc.)
-def line_level_concordance_search(infile_name, search_term):
+def line_level_concordance_search(infile_name, search_term, mode):
     try:
         infile = open(infile_name, 'r')
         # turn the whole thing into one big list
@@ -28,12 +28,23 @@ def line_level_concordance_search(infile_name, search_term):
         i = 0
         hits = []
         results = []
-        for line in lines:
-            if any(a_term.lower() in line.lower() for a_term in search_term):
-                print('found in line:', i, line)
-                hits.append(i)
-            i += 1
-        print(hits)
+        if mode == "any":
+            for line in lines:
+                if any(a_term.lower() in line.lower() for a_term in search_term):
+                    hits.append(i)
+                i += 1
+        elif mode == "all":
+            for line in lines:
+                if all(a_term.lower() in line.lower() for a_term in search_term):
+                    hits.append(i)
+                i += 1
+        elif mode == "none":
+            for line in lines:
+                if not(any(a_term.lower() in line.lower() for a_term in search_term)):
+                    hits.append(i)
+                i += 1
+        else:
+            print('enter valid mode value: any, all, or none')
         for hit in hits:
 #             adjust the amount of text scraped surrounding the result by changing the lines list index below
             result = (lines[(hit - 5):(hit + 20)])
@@ -42,9 +53,7 @@ def line_level_concordance_search(infile_name, search_term):
                 results_string = results_string + str(item) + " "
             results_string = re.sub("\\\\", "", results_string)
             results.append({'volume': infile_name.split("/")[1], 'line' : hit, 'text':results_string})
-        headers = ['volume','line', 'text']
-        rows = results
-        return rows
+        return results
     except FileNotFoundError:
         pass
 
